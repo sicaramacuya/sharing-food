08b7c0b73b89cf38102ec8f670a60fb9a964f57f
sicaramacuya <eric.morales-rodriguez@students.makeschool.com>
2020-12-02 11:16:49 -0400

    adds donate template and route

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from flask import Flask,request, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
+def home_page():
     """A homepage with the description of the problem and solution."""
     return render_template('home.html')
 
+@app.route('/donate')
+def donate_page():
+    """A page where the user can donate food."""
+    return render_template('donate.html')
+    
 if __name__ == '__main__':
     app.run(debug=True)
